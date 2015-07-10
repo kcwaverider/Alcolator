d4bae1a9d41ea7cb85c773488b24f64b1c8e65e9
Chad Clayton <chad@kcwaverider.com>
2015-07-09 22:03:08 -0500

    Completed the Alcolator storyboard

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Chad Clayton on 7/9/15.
+//  Copyright (c) 2015 Chad Clayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
